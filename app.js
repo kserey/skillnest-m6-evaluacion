16b9548801cbbcc2f109704c23cc99c119316236
IrinaSerey <irina.serey@gmail.com>
2025-12-06 19:54:04 -0300

    feat: add routing for contactos to handle requests

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use('/', contactosRoutes);
 
 
 app.listen(PORT, () => {
