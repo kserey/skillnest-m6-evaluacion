eed302e82faaf19be5f2a5bbf261240560391309
IrinaSerey <irina.serey@gmail.com>
2025-12-06 20:00:08 -0300

    feat: add 404 error handling view and update app.js for routing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const path = require('path');
 
+// 1. Importamos las rutas (Esto estaba bien)
 const contactosRoutes = require('./routes/contactos');
 
 const app = express();
@@ -15,8 +16,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', contactosRoutes);
+app.use('/', contactosRoutes); 
 
+app.use((req, res, next) => {
+    res.status(404).render('404');
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
